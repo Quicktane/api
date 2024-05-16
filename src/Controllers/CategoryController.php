c7bd21f3366f60957cc501068d3b3e56f0e518b3
Kanarskiy <kanarskiytm@gmail.com>
2024-05-17 01:21:50 +0300

    added categories

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -3,15 +3,12 @@
 namespace Quicktane\Api\Controllers;
 
 use Quicktane\Api\Requests\CreateCategoryRequest;
-use Quicktane\Core\Models\Category;
-use Quicktane\Core\Services\CategoryService;
+use Quicktane\Core\Category\Services\CategoryService;
 
 class CategoryController
 {
     public function create(CreateCategoryRequest $request, CategoryService $service)
     {
-        $category = $service->create(new Category($request->validated()));
 
-        return $category;
     }
 }
